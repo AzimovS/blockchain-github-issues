c3a67a7532d08c90993559cf782e21d735dff6ca
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-09-15 23:13:51 +0200

    feat: add cron to fetch issues

---
+++ packages/nextjs/app/api/cron/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { fetchIssues } from "~~/app/issues";
+import Issue from "~~/lib/models/Issue";
+
+function MoreThanOneHour(inputDate: Date): boolean {
+  const currentTime = new Date();
+  const timeDiff = currentTime.getTime() - inputDate.getTime();
+  // Convert time difference from milliseconds to hours
+  const timeDiffInHours = timeDiff / (1000 * 60 * 60);
+  return timeDiffInHours > 1;
+}
+
+export const GET = async () => {
+  const latestIssue = await Issue.findOne().sort({ savedAt: -1 }).exec();
+  if (MoreThanOneHour(latestIssue?.savedAt)) {
+    fetchIssues();
+    return NextResponse.json({ message: "Successfully fetched" });
+  }
+  return NextResponse.json({ message: "The last fetch was less than one hour" });
+};
