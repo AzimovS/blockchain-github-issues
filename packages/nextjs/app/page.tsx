c3f16d7279793c3e06c9bd172ed8ae060c6fca0b
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-09-18 17:07:30 +0200

    fix: code refactoring

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -12,7 +12,6 @@ import { ITEMS_PER_PAGE } from "~~/utils/const";
 import { getFilterCounts, getIssues } from "~~/utils/getIssues";
 
 const Home: NextPage = () => {
-  // const [issues, setIssues] = useState<Issue[]>([]);
   const [filteredIssues, setFilteredIssues] = useState<Issue[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [currentPage, setCurrentPage] = useState(1);
@@ -29,6 +28,11 @@ const Home: NextPage = () => {
     }
   };
 
+  const updateIssues = async (newIssues: Issue[]) => {
+    setFilteredIssues(newIssues);
+    // You cannot await setState, so code here will run before the state is updated
+  };
+
   const fetchFiltersCounts = async (filterVals: FilterValues) => {
     const res: IssueMetadataCounts = await getFilterCounts(filterVals);
     if (filterVals?.labels.length === 0 && filterVals?.languages.length === 0) {
@@ -43,90 +47,71 @@ const Home: NextPage = () => {
   };
 
   const fetchIssues = async (page: number, updateTotalPages: boolean) => {
-    const issuesData = await getIssues({ page: page - 1, filterValues });
-    // setIssues(issuesData?.latestIssues);
-    setFilteredIssues(issuesData?.latestIssues);
+    const { latestIssues, totalItems } = await getIssues({ page: page - 1, filterValues });
     if (updateTotalPages) {
-      setTotalPages(Math.ceil(issuesData?.totalItems / ITEMS_PER_PAGE));
+      setTotalPages(Math.ceil(totalItems / ITEMS_PER_PAGE));
     }
+    await updateIssues(latestIssues);
   };
 
   useEffect(() => {
-    fetchIssues(1, true);
-    setIsLoading(false);
-    fetchFiltersCounts(filterValues);
+    const initializeData = async () => {
+      setIsLoading(true);
+      await fetchIssues(1, true);
+      await fetchFiltersCounts(filterValues);
+      setIsLoading(false);
+    };
+    initializeData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
-    setIsLoading(true);
-    fetchIssues(currentPage, currentPage === 1);
-    setIsLoading(false);
+    const updateIssues = async () => {
+      setIsLoading(true);
+      await fetchIssues(currentPage, currentPage === 1);
+      setIsLoading(false);
+    };
+    updateIssues();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentPage]);
 
   useEffect(() => {
-    setIsLoading(true);
-    fetchFiltersCounts(filterValues);
-    if (currentPage !== 1) {
-      setCurrentPage(1);
-    } else {
-      fetchIssues(currentPage, true);
-    }
-    setIsLoading(false);
+    const updateFilters = async () => {
+      setIsLoading(true);
+      await fetchFiltersCounts(filterValues);
+      if (currentPage !== 1) {
+        setCurrentPage(1);
+      } else {
+        await fetchIssues(currentPage, true);
+      }
+      setIsLoading(false);
+    };
+    updateFilters();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filterValues]);
 
   const handleFilterChange = (filterKey: string, newVal: string) => {
-    setIsLoading(true);
-    if (filterKey === "noAssignee") {
-      setFilterValues(prevValues => ({ ...prevValues, noAssignee: !filterValues?.noAssignee }));
-    } else if (filterKey === "languages") {
-      console.log("languages changed");
-      setFilterValues(prevValues => ({ ...prevValues, languages: newVal }));
-    } else if (filterKey === "labels") {
-      setFilterValues(prevValues => ({ ...prevValues, labels: newVal }));
-    }
-    setIsLoading(false);
+    setFilterValues(prevValues => ({
+      ...prevValues,
+      [filterKey]: filterKey === "noAssignee" ? !prevValues.noAssignee : newVal,
+    }));
   };
 
   return (
     <>
       <div className="p-6 font-sans">
-        <div className="flex items-center flex-col">
-          {/* <button className="btn btn-primary" onClick={() => fetchIssuesFromDB()}>
-            HI
-          </button>
-          <button className="btn btn-primary" onClick={() => checkRateLimit()}>
-            Rate limit
-          </button>
-          <button
-            className="btn btn-primary"
-            onClick={async () => {
-              const res = await getTotalClicks();
-              console.log(res);
-            }}
-          >
-            Get clickedLinks
-          </button>  */}
-        </div>
         <div className="flex flex-col lg:flex-row">
           <div className="lg:fixed lg:ml-4 lg:top-20 lg:left-0 lg:h-auto lg:w-96 lg:flex-shrink-0">
-            {issueMetadataCounts && (
-              <FilterBar issueMetadataCounts={issueMetadataCounts} handleChange={handleFilterChange} />
-            )}
+            <FilterBar issueMetadataCounts={issueMetadataCounts} handleChange={handleFilterChange} />
           </div>
           <div className="lg:ml-96 space-y-4 w-full">
-            {filteredIssues.length === 0 || isLoading ? (
-              <>
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <IssueCardSkeleton key={index} />
-                ))}
-              </>
+            {isLoading || filteredIssues?.length === 0 ? (
+              Array.from({ length: 5 }).map((_, index) => <IssueCardSkeleton key={index} />)
             ) : (
               <>
-                {filteredIssues.length > 0 &&
-                  filteredIssues.map((issue: Issue, index: number) => <IssueCard key={index} issue={issue} />)}
+                {filteredIssues.map((issue, index) => (
+                  <IssueCard key={index} issue={issue} />
+                ))}
                 <Pagination currentPage={currentPage} totalPages={totalPages} handlePageChange={handlePageChange} />
               </>
             )}
